e2f6a00c899b03be4af57d31753f6f49795ce0c8
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2023-06-01 00:44:09 -0700

    Reverse a string with and without using python builtin function

---
+++ reverse.py
@@ -0,0 +1,26 @@
+def reverseWords(s):
+    res = ""
+    i = 0
+    n = len(s)
+
+    while i < n:
+        while i < n and s[i] == " ":
+            i += 1
+        if i >= n:
+            break
+        j = i + 1
+        while j < n and s[j] != " ":
+            j += 1
+        word = s[i:j]
+
+        if len(res)== 0:
+            res = word
+        else:
+            res = word + " " + res
+        i = j + 1
+    return res
+
+s = ' hello world '
+
+print(reverseWords(s))
+
