e294c555cc211b288596fe79ba5984e85f46f62b
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2025-11-06 14:21:54 -0800

    Add minCost method to Solution class
    Implement minCost method to calculate minimum cost.

---
+++ 1578MinimumTime.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        total = 0
+        for i in range(1,len(neededTime)):
+            if colors[i] == colors[i-1]:
+                total += min(neededTime[i], neededTime[i-1])
+                neededTime[i] = max(neededTime[i],neededTime[i-1])
+        return total        
