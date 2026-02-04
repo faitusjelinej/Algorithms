81051aa8d652c4e4444265362a704218929be5bc
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2026-02-03 21:38:41 -0800

    Implement HitCounter class to track hits

---
+++ 362DesignHitCounter.py
@@ -0,0 +1,12 @@
+class HitCounter:
+
+    def __init__(self):
+        self.hits = []
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits and self.hits[0] <= timestamp -300:
+            self.hits.pop(0)
+        return len(self.hits)    
