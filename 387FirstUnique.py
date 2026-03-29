e432de6f7257448eec938339a2e15339049256e8
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2026-03-29 01:57:23 -0700

    Implement first unique character finder

---
+++ 387FirstUnique.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = {}
+
+        for i in s:
+            count[i] = count.get(i,0) + 1
+
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+
+        return -1            
